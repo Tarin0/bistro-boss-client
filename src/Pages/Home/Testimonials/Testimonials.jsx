1ae3db2fe56c88a959d1997160c94858dd50f001
Tarin0 <tarinmostofa837@gmail.com>
2024-03-28 09:33:44 +0600

    sectiontitle added in testimonials section

---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,20 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+
+
+
+const Testimonials = () => {
+    
+
+    return (
+        <section className="my-20">
+            <SectionTitle
+                subHeading="What Our Client Say"
+                heading={'Testimonials'}
+            ></SectionTitle>
+
+      
+        </section>
+    );
+};
+
+export default Testimonials;
