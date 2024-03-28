e9ecd427813cb0f448a9fd9b82383d711f4249f1
Tarin0 <tarinmostofa837@gmail.com>
2024-03-28 12:09:40 +0600

    menu banner added

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 
 import { Helmet } from 'react-helmet-async';
+import Cover from '../Shared/Cover/Cover';
+
+import menuImg from '../../assets/menu/menu-bg.jpg'
 
 const Menu = () => {
     return (
@@ -7,6 +10,7 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
+            <Cover img={menuImg} title="our menu"></Cover>
         </div>
     );
 };
