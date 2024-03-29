bf98e72841f9aa7a8d509fb4ea35661cc4161956
Tarin0 <tarinmostofa837@gmail.com>
2024-03-29 15:50:07 +0600

    custom hook added

--- src/Components/SectionTitle/FoodCard/FoodCard.jsx
+++ src/Components/SectionTitle/FoodCard/FoodCard.jsx
@@ -1,6 +1,8 @@
 import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useCart from "../../hooks/useCart";
 
 
 const FoodCard = ({ item }) => {
@@ -8,6 +10,8 @@ const FoodCard = ({ item }) => {
     const { user } = useAuth();
     const navigate = useNavigate();
     const location = useLocation();
+    const axiosSecure = useAxiosSecure();
+    const [, refetch] = useCart();
 
     const handleAddToCart = () => {
         if (user && user.email) {
--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
 import { FaShoppingCart } from 'react-icons/fa';
-// import useCart from "../../../hooks/useCart";
+import useCart from "../../../hooks/useCart";
 
 const NavBar = () => {
     const { user, logOut } = useContext(AuthContext);
-    // const [cart] = useCart();
+    const [cart] = useCart();
 
     const handleLogOut = () => {
         logOut()
@@ -23,7 +23,7 @@ const NavBar = () => {
             <Link to="/dashboard/cart">
                 <button className="btn">
                     <FaShoppingCart className="mr-2"></FaShoppingCart>
-                    <div className="badge badge-secondary">+8</div>
+                    <div className="badge badge-secondary">+{cart.length}</div>
                 </button>
             </Link>
         </li>
